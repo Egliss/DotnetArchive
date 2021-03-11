1bf4ae2b8c56dd4e8aafe6d0627362755c694547
Egliss <egoparadise.rest@gmail.com>
2021-03-11 09:59:57 +0900

    Update DI with IZipArchive

--- DotnetArchive/ArchiveCommand.cs
+++ DotnetArchive/ArchiveCommand.cs
@@ -1,24 +1,22 @@
 ﻿using ConsoleAppFramework;
-using GlobExpressions;
+using DotnetArchive.Archives;
 using Microsoft.Extensions.Logging;
-using System.IO;
-using System.IO.Compression;
-using System.Linq;
-using ZLogger;
+using System.Threading.Tasks;
 
 namespace DotnetArchive
 {
-    class ArchiveCommand : ConsoleAppBase
+    public class ArchiveCommand : ConsoleAppBase
     {
         private readonly ILogger<ArchiveCommand> logger;
+        private readonly IZipArchive archive;
 
-        public ArchiveCommand(ILogger<ArchiveCommand> logger)
+        public ArchiveCommand(ILogger<ArchiveCommand> logger, IZipArchive archive)
         {
             this.logger = logger;
         }
 
         [Command("zip", "generate zip archive.")]
-        public void Zip(
+        public async Task Zip(
             [Option("i", "input file or directory")] string input,
             [Option("p", "input directory based glob pattern")] string pattern = "*",
             [Option("o", "output zip file with extension")] string output = "output.zip",
@@ -27,45 +25,7 @@ namespace DotnetArchive
             [Option("q", "quiet infomation message")] bool quiet = false
             )
         {
-            // Configuration
-            var defaultLogLevel = quiet ? LogLevel.Debug : LogLevel.Information;
-            var options = 0 + (ignoreCase ? GlobOptions.CaseInsensitive : 0);
-
-            var files = Glob.Files(input, pattern, options).ToArray();
-            if(File.Exists(output))
-            {
-                File.Delete(output);
-            }
-
-            using var zip = ZipFile.Open(output, ZipArchiveMode.Update);
-
-            long processedCount = 0;
-            long skipCount = 0;
-            foreach(var item in files)
-            {
-                // hidden file
-                if(excludeHidden)
-                {
-                    var isHidden = ((int)File.GetAttributes(item)) & ((int)FileAttributes.Hidden);
-                    if(isHidden > 0)
-                        continue;
-                }
-                try
-                {
-                    zip.CreateEntryFromFile(item, item);
-                }
-                catch(IOException ex) when(ex.HResult == -2147024864)
-                {
-                    skipCount++;
-                    this.logger.ZLogWarning("[Skip] {0} using by other process.", item);
-                }
-                processedCount++;
-                this.logger.ZLog(defaultLogLevel, item);
-            }
-
-            this.logger.ZLog(defaultLogLevel, " ");
-            this.logger.ZLog(defaultLogLevel, "All file archived.");
-            this.logger.ZLog(defaultLogLevel, "Processed: {0} Skip: {1}", processedCount, skipCount);
+            await this.archive.ZipAsync(input, pattern, output, excludeHidden, ignoreCase, quiet);
         }
     }
 }
---
+++ DotnetArchive/Archives/DefaultZipArchive.cs
@@ -0,0 +1,64 @@
+﻿using GlobExpressions;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+using ZLogger;
+
+namespace DotnetArchive.Archives
+{
+    public class DefaultZipArchive : IZipArchive
+    {
+        private readonly ILogger<DefaultZipArchive> logger;
+        public DefaultZipArchive(ILogger<DefaultZipArchive> logger)
+        {
+            this.logger = logger;
+        }
+
+        public Task ZipAsync(string input, string pattern, string output, bool excludeHidden, bool ignoreCase, bool quiet)
+        {
+            // Configuration
+            var defaultLogLevel = quiet ? LogLevel.Debug : LogLevel.Information;
+            var options = 0 + (ignoreCase ? GlobOptions.CaseInsensitive : 0);
+
+            var files = Glob.Files(input, pattern, options).ToArray();
+            if(File.Exists(output))
+            {
+                File.Delete(output);
+            }
+
+            using var zip = ZipFile.Open(output, ZipArchiveMode.Update);
+
+            long processedCount = 0;
+            long skipCount = 0;
+            foreach(var item in files)
+            {
+                // hidden file
+                if(excludeHidden)
+                {
+                    var isHidden = ((int)File.GetAttributes(item)) & ((int)FileAttributes.Hidden);
+                    if(isHidden > 0)
+                        continue;
+                }
+                try
+                {
+                    zip.CreateEntryFromFile(item, item);
+                }
+                catch(IOException ex) when(ex.HResult == -2147024864)
+                {
+                    skipCount++;
+                    this.logger.ZLogWarning("[Skip] {0} using by other process.", item);
+                }
+                processedCount++;
+                this.logger.ZLog(defaultLogLevel, item);
+            }
+
+            this.logger.ZLog(defaultLogLevel, " ");
+            this.logger.ZLog(defaultLogLevel, "All file archived.");
+            this.logger.ZLog(defaultLogLevel, "Processed: {0} Skip: {1}", processedCount, skipCount);
+
+            return Task.CompletedTask;
+        }
+    }
+}
