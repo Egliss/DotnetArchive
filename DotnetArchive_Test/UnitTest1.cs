09f7bbf2a990d75e4c2768da43e57330eb858138
Egliss <egoparadise.rest@gmail.com>
2021-03-11 10:00:29 +0900

    Add Initialize and Cleanup

--- DotnetArchive_Test/UnitTest1.cs
+++ DotnetArchive_Test/UnitTest1.cs
@@ -1,13 +1,41 @@
+using ConsoleAppFramework;
+using DotnetArchive;
+using Microsoft.Extensions.Hosting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace DotnetArchive_Test
 {
     [TestClass]
-    public class UnitTest1
+    public class ArchiveTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestInitialize]
+        public void Initialize()
         {
+            Directory.CreateDirectory("Test");
+            File.CreateText("Test");
+            // SingleFile
+            Directory.CreateDirectory("Test/A");
+            File.CreateText("Test/A");
+            // Empty
+            Directory.CreateDirectory("Test/B");
+            // MultiFile
+            Directory.CreateDirectory("Test/C");
+            File.CreateText("Test/C_0");
+            File.CreateText("Test/C_1");
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Directory.Delete("Test", true);
+        }
+
+        [TestMethod]
+        public async Task Zip()
+        {
+            await Host.CreateDefaultBuilder()
+                .RunConsoleAppFrameworkAsync<ArchiveCommand>(args);
         }
     }
 }
