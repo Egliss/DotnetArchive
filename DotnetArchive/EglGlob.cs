739121a938d099b657d0eed91296b1f3da2d2477
Egliss <egoparadise.rest@gmail.com>
2021-03-12 18:16:25 +0900

    Refactor Glob option struct

--- DotnetArchive/EglGlob.cs
+++ DotnetArchive/EglGlob.cs
@@ -8,27 +8,40 @@ namespace DotnetArchive
 {
     public static class EglGlob
     {
-        public static IEnumerable<string> Run(string input, string pattern, string excludePattern, bool excludeHidden, bool ignoreCase)
+        public struct Options
         {
-            var options = 0 + (ignoreCase ? GlobOptions.CaseInsensitive : 0);
+            // glob root directory
+            public string input { get; set; }
+            // glob input pattern
+            public string pattern { get; set; }
+            // glob exclude pattern
+            public string excludePattern { get; set; }
+            // exclude file hidden attributed 
+            public bool excludeHidden { get; set; }
+            // ignore word case of pattern matching
+            public bool ignoreCase { get; set; }
+        }
 
-            var files = Glob.Files(input, pattern, options).ToArray();
+        public static IEnumerable<string> Run(Options option)
+        {
+            var options = 0 + (option.ignoreCase ? GlobOptions.CaseInsensitive : 0);
+            var files = Glob.Files(option.input, option.pattern, options).ToArray();
 
-            var hideDirectories = Glob.Directories(input, pattern, options)
-                .Select(m => Path.Combine(input, m))
+            var hideDirectories = Glob.Directories(option.input, option.pattern, options)
+                .Select(m => Path.Combine(option.input, m))
                 .Where(m => (new DirectoryInfo(m).Attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
                 .Select(m => new Regex(m.Replace('\\', '/') + "*"))
                 .ToArray();
             var excludeFiles = Glob
-                .Files(input, excludePattern, options)
-                .Select(m => Path.Combine(input, m).Replace('\\', '/'))
+                .Files(option.input, option.excludePattern, options)
+                .Select(m => Path.Combine(option.input, m).Replace('\\', '/'))
                 .ToHashSet();
 
             foreach(var item in files)
             {
-                var file = Path.Combine(input, item).Replace('\\', '/');
+                var file = Path.Combine(option.input, item).Replace('\\', '/');
                 // hidden file
-                if(excludeHidden)
+                if(option.excludeHidden)
                 {
                     var directory = Path.GetDirectoryName(file).Replace('\\', '/');
                     var directoryAttribute = new DirectoryInfo(directory).Attributes;
@@ -45,6 +58,18 @@ namespace DotnetArchive
 
                 yield return item;
             }
+        }
+        public static IEnumerable<string> Run(string input, string pattern, string excludePattern, bool excludeHidden, bool ignoreCase)
+        {
+            var options = new Options()
+            {
+                input = input,
+                pattern = pattern,
+                excludePattern = excludePattern,
+                excludeHidden = excludeHidden,
+                ignoreCase = ignoreCase,
+            };
+            return EglGlob.Run(options);
         }
     }
 }
